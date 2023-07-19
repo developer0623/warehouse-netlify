f21bd786fa88ae288b9214a039e841ae1d6cbddd
developer0623 <developer0623@gmail.com>
2023-07-19 23:21:19 +0900

    test6

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -65,7 +65,7 @@ const routes: Routes = [
 ];
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules, useHash: true })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
   ],
   exports: [RouterModule]
 })
